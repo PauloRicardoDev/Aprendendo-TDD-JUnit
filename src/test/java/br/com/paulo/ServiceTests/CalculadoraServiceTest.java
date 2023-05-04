9704e9cc95da500876353bd50237ed5b35871383
PauloRicardoDev <pauloricardochagas.dev@gmail.com>
2023-05-04 15:01:45 -0300

    Quarto Commit - "Melhoria do codigo dos testes".

--- src/test/java/br/com/paulo/ServiceTests/CalculadoraServiceTest.java
+++ src/test/java/br/com/paulo/ServiceTests/CalculadoraServiceTest.java
@@ -3,29 +3,34 @@ package br.com.paulo.ServiceTests;
 import br.com.paulo.Enums.TipoDeOperacao;
 import br.com.paulo.Service.CalculadoraService;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class CalculadoraServiceTest {
+
+    private CalculadoraService calculadoraService;
+    @Before
+    public void inicializar(){
+        this.calculadoraService = new CalculadoraService();
+    }
+
     @Test
     public void deveCalcularOResultadoDaSomaDeDoisNumeros(){
-        CalculadoraService calculadoraService = new CalculadoraService();
         BigDecimal soma = calculadoraService.calcular(TipoDeOperacao.SOMA,
                 new BigDecimal("100.00"), new BigDecimal("10.00"));
         Assert.assertEquals(new BigDecimal("110.00"), soma);
     }
     @Test
     public void deveCalcularOResultadoDaSustracaoDeDoisNumeros(){
-        CalculadoraService calculadoraService = new CalculadoraService();
         BigDecimal subtracao = calculadoraService.calcular(TipoDeOperacao.SUBTRACAO,
                 new BigDecimal("20.00"), new BigDecimal("10.00"));
         Assert.assertEquals(new BigDecimal("10.00"), subtracao);
     }
     @Test
     public void deveCalcularOResultadoDaMultiplicacaoDeDoisNumeros(){
-        CalculadoraService calculadoraService = new CalculadoraService();
         BigDecimal multiplicacao = calculadoraService.calcular(TipoDeOperacao.MULTIPLICACAO,
                 new BigDecimal("10.00"), new BigDecimal("10.00"));
         Assert.assertEquals(new BigDecimal("100.00"), multiplicacao.setScale(2, RoundingMode.HALF_UP));
@@ -33,7 +38,6 @@ public class CalculadoraServiceTest {
     }
     @Test
     public void deveCalcularOResultadoDaDivisaoDeDoisNumeros(){
-        CalculadoraService calculadoraService = new CalculadoraService();
         BigDecimal divisao = calculadoraService.calcular(TipoDeOperacao.DIVISAO,
                 new BigDecimal("10.00"), new BigDecimal("10.00"));
         Assert.assertEquals(new BigDecimal("1"), divisao);
--- src/test/java/br/com/paulo/ServiceTests/ReajustePorDesenpenhoServiceTest.java
+++ src/test/java/br/com/paulo/ServiceTests/ReajustePorDesenpenhoServiceTest.java
@@ -3,34 +3,39 @@ package br.com.paulo.ServiceTests;
 import br.com.paulo.Enums.Desempenho;
 import br.com.paulo.Model.Funcionario;
 import br.com.paulo.Service.ReajustePorDesempenhoService;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
 
 public class ReajustePorDesenpenhoServiceTest {
+
+    private ReajustePorDesempenhoService reajustePorDesempenhoService;
+    private Funcionario funcionario;
+
+    @Before
+    public void inicializar(){
+        this.reajustePorDesempenhoService = new ReajustePorDesempenhoService();
+        this.funcionario = new Funcionario
+                ("Teste", "rgc123456" , "Desenvolvedor Senior Java",
+                        new BigDecimal("7000.00"));
+    }
+
     @Test
     public void naoDevereajustarFuncionarioComDesempenhoREGULAR(){
-        ReajustePorDesempenhoService rpd = new ReajustePorDesempenhoService();
-        BigDecimal valorReajustado = rpd.reajuste(Desempenho.REGULAR, new Funcionario
-                ("Teste", "rgc123456" , "Desenvolvedor Senior Java",
-                        new BigDecimal("7000.00")));
+        BigDecimal valorReajustado = reajustePorDesempenhoService.reajuste(Desempenho.REGULAR, funcionario);
         Assert.assertEquals(new BigDecimal("7000.00"), valorReajustado);
     }
     @Test
     public void reajustandoFuncionarioComDesempenhoBOM(){
-        ReajustePorDesempenhoService rpd = new ReajustePorDesempenhoService();
-        BigDecimal valorReajustado = rpd.reajuste(Desempenho.BOM, new Funcionario
-                ("Teste", "rgc123456" , "Desenvolvedor Senior Java",
-                        new BigDecimal("6000.00")));
+        BigDecimal valorReajustado = reajustePorDesempenhoService.reajuste(Desempenho.BOM, funcionario);
         Assert.assertEquals(new BigDecimal("6600.00"), valorReajustado);
     }
     @Test
     public void reajustandoFuncionarioComDesempenhoEXELENTE(){
-        ReajustePorDesempenhoService rpd = new ReajustePorDesempenhoService();
-        BigDecimal valorReajustado = rpd.reajuste(Desempenho.EXECELENTE, new Funcionario
-                ("Teste", "rgc123456" , "Desenvolvedor Senior Java",
-                        new BigDecimal("8000.00")));
+        BigDecimal valorReajustado = reajustePorDesempenhoService.reajuste(Desempenho.EXECELENTE, funcionario);
         Assert.assertEquals(new BigDecimal("9600.00"), valorReajustado);
     }
 }
